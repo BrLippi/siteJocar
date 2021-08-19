bc7b718167689602495475fdb2c8c6f18a04cce2
BrLippi <brunolippi@outlook.com>
2021-08-19 09:52:28 -0300

    criação do teste com o resultado

--- src/test/java/Driver.java
+++ src/test/java/Driver.java
@@ -1,62 +1,21 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 
-import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
-    String url;
-    WebDriver driver;
 
+    public WebDriver Webdriver() {
 
-    @Before
-
-    public void start() {
-
-        url = "https://jocar.com.br";
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\Bruno\\IdeaProjects\\siteJocar\\drivers\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(50000, TimeUnit.MILLISECONDS); //tempo para encontra elementos
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.jocar.com.br");
+        driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
         driver.manage().window().maximize();
+        return driver;
 
     }
 
-    @Test
-
-
-    public void test() {
-        driver.get(url);
-        WebElement searchbox = driver.findElement(By.id("ctl00_ctl00_mstContentMain_mstCabecalho_CabBusca1_txtDescricao"));
-        searchbox.sendKeys("Óleo do motor");
-        WebElement searchbutton = driver.findElement(By.id("btnBuscarCabBusca"));
-        searchbutton.click();
-        WebElement searchResult = driver.findElement(By.className("productName"));
-        String result = searchResult.getText();
-        result = result.toLowerCase(Locale.ROOT);
-
-
-        if (result.contains("óleo") || result.contains("Oleo")) {
-            System.out.println("Sucesso");
-        } else {
-            System.out.println("ERRO");
-        }
-
-    }
-
-
-    @After
-
-
-    public void end() {
-
-        driver.quit();
-    }
 }
